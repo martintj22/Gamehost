4e737ffcf791704fbd1fb6da472a7e7325b13217
Fumlefinger <jann2273>
2018-04-20 19:55:14 +0200

    style folder and style.css has been added and style.css has been linked via header.php

--- Web/www/header.php
+++ Web/www/header.php
@@ -4,7 +4,8 @@ require_once("sql/conn.php");
 
 <html>
     <head>
-
+        <link rel="stylesheet" type="text/css" href="theme.css">
+        <title>NoobNice Serverhosting</title>
     </head>
 
     <body>
