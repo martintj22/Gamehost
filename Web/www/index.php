a28d07794fe33137c5a094bee0af631333a1580a
DESKTOP-UT8VBH4\Satoz <satoz.game@gmail.com>
2018-04-22 23:29:28 +0200

    Css box sizes ready

--- Web/www/index.php
+++ Web/www/index.php
@@ -15,17 +15,20 @@ include_once("header.php");
         <!--This module show the most Popular services hosted by us-->
         <!--Contains 3 boxes and a title, each box contains a image with the size 500x140..-->
 
-        <div id="serviceLeft">
+     
+
+        <div class="service">
             <!--We need a picture from the database...-->
         </div>
 
-        <div id="serviceMiddle">
+		<div class="service">
             <!--We need a picture from the database...-->
         </div>
 
-        <div id="serviceRight">
+		<div class="service">
             <!--We need a picture from the database...-->
         </div>
+
 
     </div> <!--popularServices ends here...-->
 
@@ -33,6 +36,20 @@ include_once("header.php");
         <!--This is a button, that hides popularServices and shows all services we offer.-->
 		<h1> Se flere </h1>
     </div>
+
+	<div class="detail">
+	<h1> Why us </h1>
+	</div>
+
+	<div class="detail">
+		<h1> Why us </h1>
+
+	</div>
+
+	<div class="detail">
+		<h1> Why us </h1>
+
+	</div>
 
     <!--More modules can be added down below-->
 
